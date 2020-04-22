fea2e4632142fd4a5b55fb2af89aacb612f1408e
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-04-23 02:20:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,24 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const dmembed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
 client.on('ready', () => {
 
     client.user.setPresence({ activity: { name: 'with discord.js' }, status: 'online' });
