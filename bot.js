9ba81afe27a2bcf72b084c4fce0c6281c2e640a7
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-04-22 22:47:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
 
     client.user.setStatus('online');
 
-    client.user.setGame('Minecraft is The Best');
+    client.user.setActivity('Minecraft is The Best');
 
     //client.user.setActivity('discord.js', { type: 'WATCHING' })
     //.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
