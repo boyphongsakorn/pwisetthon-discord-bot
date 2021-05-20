e8bb5783bff5639153e7395c1b41e3c2fa71b38a
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-21 02:11:51 +0700

    test

--- webcap.js
+++ webcap.js
@@ -1,4 +1,7 @@
 const Pageres = require('pageres');
+const fetch = require('node-fetch');
+var http = require('http');
+var fs = require('fs');
 
 function padLeadingZeros(num, size) {
     var s = num+"";
@@ -13,18 +16,29 @@ let year = new Date().getFullYear();
 date = padLeadingZeros(date, 2);
 month = padLeadingZeros(month, 2);
 
-var http = require('http');
-var fs = require('fs');
-
 http.createServer(function (req, res) {
-    (async () => {
-        await new Pageres({delay: 5,filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run']}})
-            .src('https://lottsanook.vercel.app/api/viewlot.php', ['1600x1066'])
-            .dest(__dirname)
-            .run();
-    
-        console.log('Finished generating screenshots!');
-        res.writeHead(200,{'content-type':'image/png'});
-        fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
-    })();
+    let url = "https://lottsanook.vercel.app/api/?date="+date+""+month+""+year;
+
+    let settings = { method: "Get" };
+
+    fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        if(json[0][1] == "0" || json[0][1] == 0 || json[0][1] == "XXXXXX"){
+            (async () => {
+                await new Pageres({delay: 5,filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run']}})
+                    .src('https://lottsanook.vercel.app/api/viewlot.php', ['1600x1066'])
+                    .dest(__dirname)
+                    .run();
+            
+                console.log('Finished generating screenshots!');
+                res.writeHead(200,{'content-type':'image/png'});
+                fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
+            })();
+        }else{
+            console.log('right now can\'t screenshots!');
+            res.writeHead(200,{'content-type':'image/png'});
+            fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
+        }
+    });
 }).listen(process.env.PORT);
