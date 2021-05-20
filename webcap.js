6431f0c486f5acf64fedf15a5ee4d1c8009f9b68
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-21 02:59:53 +0700

    test

--- webcap.js
+++ webcap.js
@@ -17,28 +17,14 @@ date = padLeadingZeros(date, 2);
 month = padLeadingZeros(month, 2);
 
 http.createServer(function (req, res) {
-    let url = "https://lottsanook.vercel.app/api/?date="+date+""+month+""+year;
-
-    let settings = { method: "Get" };
-
-    fetch(url, settings)
-    .then(res => res.json())
-    .then((json) => {
-        if(json[0][1] == "0" || json[0][1] == 0 || json[0][1] == "XXXXXX"){
-            (async () => {
-                await new Pageres({filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run']}})
-                    .src('https://lottsanook.vercel.app/api/viewlot.php', ['1600x1066'])
-                    .dest(__dirname)
-                    .run();
+    (async () => {
+        await new Pageres({filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run']}})
+            .src('https://lottsanook.vercel.app/api/viewlot.php', ['1600x1066'])
+            .dest(__dirname)
+            .run();
             
-                console.log('Finished generating screenshots!');
-                res.writeHead(200,{'content-type':'image/png'});
-                fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
-            })();
-        }else{
-            console.log('right now can\'t screenshots!');
-            res.writeHead(200,{'content-type':'image/png'});
-            fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
-        }
-    });
+        console.log('Finished generating screenshots!');
+        res.writeHead(200,{'content-type':'image/png'});
+        fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
+    })();
 }).listen(process.env.PORT);
