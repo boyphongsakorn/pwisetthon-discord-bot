a9b38cf1b0b6daa49a131ef787a528f6716b2aa3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-20 23:51:29 +0700

    test

---
+++ webcap.js
@@ -0,0 +1,34 @@
+const Pageres = require('pageres');
+
+// functions
+
+function padLeadingZeros(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+// end functions
+
+let date = new Date().getDate();
+let month = new Date().getMonth()+1;
+let year = new Date().getFullYear();
+
+date = padLeadingZeros(date, 2);
+month = padLeadingZeros(month, 2);
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    (async () => {
+        await new Pageres({delay: 5,filename: "<%= date %>"})
+            .src('https://lottsanook.herokuapp.com/viewlot.php', ['1600x1066'])
+            .dest(__dirname)
+            .run();
+    
+        console.log('Finished generating screenshots!');
+        res.writeHead(200,{'content-type':'image/png'});
+        fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
+    })();
+}).listen(8080);
