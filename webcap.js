b0180a02059a44bed3f9798a07a8e6f8129141bc
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-23 21:44:45 +0700

    test

--- webcap.js
+++ webcap.js
@@ -29,7 +29,7 @@ http.createServer(function (req, res) {
             console.error(err)
         }
 
-        await new Pageres({filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run']}})
+        await new Pageres({filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run','--disable-extensions'], executablePath: '/usr/bin/chromium-browser'}})
             .src('https://lottsanook.vercel.app/api/viewlot.php', ['1600x1066'])
             .dest(__dirname)
             .run();
