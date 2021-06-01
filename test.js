fd7054cefaeede70a0054f87484e1686fef50371
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-06-01 19:48:35 +0700

    test

---
+++ test.js
@@ -0,0 +1,57 @@
+const Pageres = require('pageres');
+var http = require('http');
+var fs = require('fs');
+const request = require('request');
+
+function padLeadingZeros(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+let date = new Date().getDate();
+let month = new Date().getMonth()+1;
+let year = new Date().getFullYear();
+
+date = padLeadingZeros(date, 2);
+month = padLeadingZeros(month, 2);
+
+http.createServer(function (req, res) {
+    (async () => {
+
+        let test = {}
+
+        var options = {
+            'method': 'GET',
+            'url': 'https://lotapi.pwisetthon.com/lastlot',
+            'json': true,
+            'headers': {
+            }
+        };
+
+        request(options, async function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body.win);
+            test = response.body
+
+            try {
+                if (fs.existsSync(year+'-'+month+'-'+date+'.png')) {
+                    fs.unlinkSync(year+'-'+month+'-'+date+'.png')
+                }
+                //file removed
+            } catch(err) {
+                //console.error(err)
+            }
+
+            await new Pageres({filename: "<%= date %>",launchOptions: {args: ['--no-sandbox','--disable-setuid-sandbox','--no-first-run','--disable-extensions']}})
+                .src('data:text/html,<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous"><link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet"><style>body{font-family: \'Mitr\', sans-serif;background-image: url(\'https://lotpost.pwisetthon.com/fbbg.png\');color: white;}</style></head><h1 style="margin-top: 150px;margin-left: 180px;font-size: 80px;">ผลรางวัลสลากกินแบ่งรัฐบาล</h1><h2 style="margin-top: 15px;margin-left: 0px;font-size: 50px;margin-right: 590px;text-align: right;">เมื่อประจำวันที่ </h2><h2 style="margin-top: 50px;font-size: 80px;margin-left: 450px;">รางวัลที่ 1</h2><h2 style="font-size: 12.25vw;margin-left: 190px;margin-top: -40px;margin-right: 650px;text-align: center;">'+test.win+'</h2><h2 style="margin-left: 1095px;margin-top: -255px;font-size: 50px;">เลขท้าย สองตัว</h2><h2 style="margin-left: 1120px;font-size: 150px;margin-top: -10px;">'+test.twoend+'</h2><h2 style="margin-top: 25px;margin-left: 325px;font-size: 60px;">เลขหน้า สามตัว</h2><h2 style="font-size: 100px;margin-left: 260px;">'+test.threefirst.replace(",", " | ")+'</h2><h2 style="margin-left: 875px;margin-top: -207px;font-size: 60px;">เลขท้าย สามตัว</h2><h2 style="font-size: 5.96vw;margin-left: 805px;max-width: 475px;">'+test.threeend.replace(",", " | ")+'</h2>', ['1600x1066'])
+                .dest(__dirname)
+                .run();
+
+                console.log('Finished generating screenshots!');
+                res.writeHead(200,{'content-type':'image/png'});
+                fs.createReadStream(year+'-'+month+'-'+date+'.png').pipe(res);
+        });
+
+    })();
+}).listen(3000);
