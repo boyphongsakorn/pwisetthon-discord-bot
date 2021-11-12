96e4595f6997a44679afb1de3894742cc8661868
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-12 23:15:29 +0700

    test

--- bot.js
+++ bot.js
@@ -173,7 +173,7 @@ client.on("guildCreate", guild => {
     commands?.create({
         name: 'srchlot',
         description: "ตรวจสลากฯ ล่าสุดด้วยเลข",
-        options: {
+        options?: {
             type: require('discord.js').Constants.ClientApplicationAssetTypes.NUMBER,
             name: 'number',
             description: 'ตัวเลขที่ต้องการตรวจสลากฯ',
