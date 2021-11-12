de156132da626b0d2fc8835b42478378349c7435
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-12 22:24:36 +0700

    test

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord,{Intents} = require('discord.js');
 const DS = require("discord-slash-commands-client");
 const cron = require("cron");
 const fetch = require('node-fetch');
@@ -10,7 +10,7 @@ var http = require('http');
 require('dotenv').config();
 
 //const client = new Discord.Client({fetchAllMembers: true}); only for get all server and member
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const DSclient = new DS.Client(
     process.env.BOT_TOKEN,
