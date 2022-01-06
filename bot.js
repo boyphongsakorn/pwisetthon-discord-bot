4daf70b7ccfaa6d9e78f1e5fe8b95822f92e2655
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-01-06 23:57:42 +0700

    ok

--- bot.js
+++ bot.js
@@ -752,7 +752,7 @@ client.on('interactionCreate', async interaction => {
                     console.log('Waiting for image to be downloaded');
                 }*/
                 
-                try {
+                /*try {
                     fs.access('./lottery_'+body.info.date+'.png', fs.F_OK, async (err) => {
                         if (err) {
                             //console.error(err)
@@ -777,7 +777,20 @@ client.on('interactionCreate', async interaction => {
                 while (!fs.existsSync('./lottery_'+body.info.date+'.png')) {
                     console.log('Waiting for image to be downloaded');
                     console.log(fs.existsSync('./lottery_'+body.info.date+'.png'))
-                    console.log('./lottery_'+body.info.date+'.png')
+                }*/
+
+                fetch('http://192.168.31.210:4000/?date=' + body.info.date)
+                .then(res =>
+                    res.body.pipe(fs.createWriteStream('./lottery_'+body.info.date+'.png'))
+                )
+
+                //for 10 seconds
+                for (let i = 0; i < 10; i++) {
+                    if(fs.existsSync('./lottery_'+body.info.date+'.png')){
+                        console.log('Image downloaded');
+                        break;
+                    }
+                    await sleep(1000);
                 }
 
                 const file = new MessageAttachment('./lottery_'+body.info.date+'.png');
