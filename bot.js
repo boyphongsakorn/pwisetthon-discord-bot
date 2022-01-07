66b210145d9e77db877e045d49107f48b047b392
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-01-07 15:51:16 +0700

    ok

--- bot.js
+++ bot.js
@@ -857,9 +857,8 @@ client.on('interactionCreate', async interaction => {
         //console.log(interaction.options.getString('number'));
         let numbertofind = interaction.options.getString('number');
         if(interaction.options.getString('number') == null && interaction.commandName === 'ตรวจสลากฯ') {
-            /*let ohmygod = await interaction.channel.message.fetch(interaction.targetId);
-            numbertofind = ohmygod.content;*/
-            console.log(interaction);
+            let ohmygod = interaction.channel.message.fetch(interaction.targetId);
+            numbertofind = ohmygod.content;
         }
         console.log(numbertofind);
         await interaction.deferReply();
