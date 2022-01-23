191d55ffaefb0e493c50a8c4b506c7138c24dff1
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-01-23 19:30:15 +0700

    test

--- bot.js
+++ bot.js
@@ -1282,7 +1282,7 @@ client.on('interactionCreate', async interaction => {
                 .setFooter({ text: 'ข้อมูลจาก Database \nบอทจัดทำโดย Phongsakorn Wisetthon \nซื้อกาแฟให้ผม ko-fi.com/boyphongsakorn' });
 
             for(let i = 0; i < result.length; i++){
-                msg.addField(result[i].datetime,result[i].numberbuy,true );
+                msg.addField(result[i].numberbuy,result[i].datetime,true);
             }
 
             await interaction.editReply({ embeds: [msg] });
