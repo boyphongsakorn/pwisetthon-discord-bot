8bc91fbff3513afb81b4b90b7b4eea95dc9d92e5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-05-27 19:54:26 +0700

    test

--- bot.js
+++ bot.js
@@ -634,36 +634,36 @@ let scheduledthaioil = new cron.CronJob('1-59/3 * * * *', () => {
                         const wow = data;
                         let imagegood = false;
 
-                        while (imagegood == false) {
-                            await fetch('https://screenshot-xi.vercel.app/api?url=https://boyphongsakorn.github.io/thaioilpriceapi&width=1000&height=1000')
+
+                        await fetch('https://screenshot-xi.vercel.app/api?url=https://boyphongsakorn.github.io/thaioilpriceapi&width=1000&height=1000')
+                            .then(res => res.buffer())
+                            .then(async (res) => {
+                                await fs.writeFileSync('./lastoilprice.png', res)
+                                imagegood = true;
+                            })
+                            .catch(async (err) => {
+                                console.log(err);
+                                imagegood = false;
+                            });
+
+                        let files
+                        //let imageisgood = false
+
+                        //check if file exist and size is not 0
+                        if (fs.existsSync('./lastoilprice.png') && fs.statSync('./lastoilprice.png').size > 0) {
+                            files = new MessageAttachment('./lastoilprice.png');
+                            //imageisgood = true
+                        } else {
+                            imagegood = false;
+                            await fetch('https://topapi.pwisetthon.com/image')
                                 .then(res => res.buffer())
                                 .then(async (res) => {
                                     await fs.writeFileSync('./lastoilprice.png', res)
+                                    files = new MessageAttachment('./lastoilprice.png');
                                     imagegood = true;
                                 })
-                                .catch(async (err) => {
-                                    console.log(err);
-                                    imagegood = false;
-                                });
-
-                            let files
-                            //let imageisgood = false
-
-                            //check if file exist and size is not 0
-                            if (fs.existsSync('./lastoilprice.png') && fs.statSync('./lastoilprice.png').size > 0) {
-                                files = new MessageAttachment('./lastoilprice.png');
-                                //imageisgood = true
-                            } else {
-                                imagegood = false;
-                                await fetch('https://topapi.pwisetthon.com/image')
-                                    .then(res => res.buffer())
-                                    .then(async (res) => {
-                                        await fs.writeFileSync('./lastoilprice.png', res)
-                                        files = new MessageAttachment('./lastoilprice.png');
-                                        imagegood = true;
-                                    })
-                            }
                         }
+
 
                         let msg = new MessageEmbed()
                             .setColor('#0099ff')
