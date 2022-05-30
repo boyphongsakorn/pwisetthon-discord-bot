3886c6304914b09361416f21d632f88d56533102
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-05-31 00:07:19 +0700

    test

--- bot.js
+++ bot.js
@@ -1034,7 +1034,7 @@ client.on('interactionCreate', async interaction => {
                     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
                 });*/
                 fetch(uri).then(res => res.buffer()).then(buffer => {
-                    fs.writeFileSync(filename, buffer);
+                    fs.writeFileSync(filename, buffer).on('close', callback);
                 }).catch(err => {
                     console.log(err)
                 });
